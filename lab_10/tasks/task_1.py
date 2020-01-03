2e87b31b0f4b5769298bc40274be587502d3db2f
joasta <joanna.starobrat@fizyka.pw.edu.pl>
2020-01-03 15:11:13 +0100

    lab10-p1

--- lab_10/tasks/task_1.py
+++ lab_10/tasks/task_1.py
@@ -1,8 +1,16 @@
 import requests
-
+from urllib.parse import urljoin
+API_URL = 'https://www.metaweather.com/api/'
 
 def get_cities_woeid(query: str, timeout: float = 5.):
-    pass
+    result = {}
+    location_url = urljoin(API_URL, 'location/search')
+
+    response = requests.get(location_url, params=dict(query=query), timeout=timeout)
+    tablica = response.json()
+    for elem in tablica:
+        result[elem["title"]] = elem["woeid"]
+    return result
 
 
 if __name__ == '__main__':
@@ -12,6 +20,7 @@ if __name__ == '__main__':
         'Newark': 2459269,
     }
     try:
-        get_cities_woeid('Warszawa', 0.1)
+        get_cities_woeid('Warszawa', 0.0001)
     except Exception as exc:
+        print("error")
         isinstance(exc, requests.exceptions.Timeout)
--- lab_10/tasks/task_2.py
+++ lab_10/tasks/task_2.py
@@ -1,5 +1,9 @@
 import pathlib
 from typing import Optional, Union, List
+from urllib.parse import urljoin
+import requests
+import csv
+import os.path
 
 
 API_URL = 'https://www.metaweather.com/api/'
@@ -10,7 +14,47 @@ def get_city_data(
         path: Optional[Union[str, pathlib.Path]] = None,
         timeout: float = 5.
 ) -> (str, List[str]):
-    pass
+    files=[]
+    days = 31+1
+    emptyPath = False
+    
+    direct = str(woeid) + "_" + str(year) + "_" + f"{month:02d}"
+
+    if path is None:
+        path0 = pathlib.Path.cwd()
+        emptyPath = True
+    elif not os.path.isabs(path):
+        path0 = pathlib.Path.cwd() / path
+
+    path0 = path0 / direct
+
+    if emptyPath:
+        path1 = path0
+    else:
+        path1 = path + "/" + direct
+
+    path0.mkdir(parents=True, exist_ok=True)
+
+    for day in range(1, days):
+        location_url = urljoin(API_URL, f'location/{woeid}/{year}/{month}/{day}')
+        try:
+            response = requests.get(location_url, timeout=5)
+        except requests.exceptions.Timeout:
+            print(f'Request for: {location_url} took to long!')
+        else:
+            print(response.url)
+            data = response.json()
+            if len(data) == 0:
+                break
+            else:
+                file_name = f'{year}-{month}-{day}.csv'
+                files.append(file_name)
+                with open(path0 / file_name, 'w') as _file:
+                    writer = csv.DictWriter(_file, delimiter=',', quotechar='"', fieldnames=data[0].keys())
+                    writer.writeheader()
+                    writer.writerows(data)
+    
+    return (str(path1), files)
 
 
 if __name__ == '__main__':
@@ -25,6 +69,8 @@ if __name__ == '__main__':
     dir_path, file_paths = get_city_data(523920, 2017, 3, path='weather_data')
     assert len(file_paths) == 31
     assert pathlib.Path(dir_path).is_dir()
+    print(dir_path)
+    print(expected_path)
     assert expected_path == dir_path
 
     expected_path = 'weather_data/523920_2012_12'
@@ -32,3 +78,5 @@ if __name__ == '__main__':
     assert len(file_paths) == 0
     assert pathlib.Path(dir_path).is_dir()
     assert expected_path == dir_path
+
+    #errors
