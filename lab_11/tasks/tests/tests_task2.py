7cdd959907434e94dc44650a5e8e78b28d255290
Joanna Starobrat <Joanna.Starobrat@fizyka.pw.edu.pl>
2020-01-14 23:30:40 +0100

    lab11-draft2

--- lab_11/tasks/tests/tests_task2.py
+++ lab_11/tasks/tests/tests_task2.py
@@ -1,5 +1,22 @@
 import pytest
+from unittest.mock import Mock
 
 from lab_11.tasks.tools.metaweather import (
     get_metaweather
 )
+
+if __name__ == '__main__':
+	#request.get = Mock()
+	#request.get.raise_for_status.side_effect = [Null, requests.exceptions.HTTPError]
+	#request.get.raise_for_status = True
+
+	response_mock = Mock()
+
+	response_mock.json.return_value = {
+            'Warsaw': 523920,
+			'Newark': 2459269,
+			'Opole': 12345
+        }
+	response_mock.raise_for_status = True
+
+	requests.get.side_effect = [response_mock, Timeout]
